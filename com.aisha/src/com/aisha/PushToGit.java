7596fca043cbe4843647cb5bba3a40fdda40b30d
arvind <arvind@arvind-PC>
2021-09-20 20:35:44 +0530

    1st commit

---
+++ com.aisha/src/com/aisha/PushToGit.java
@@ -0,0 +1,12 @@
+package com.aisha;
+
+public class PushToGit 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+      System.out.println("HEY,I PUSHED MY FILE ON GIT");
+	}
+
+}
